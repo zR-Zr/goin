be6fbe0d49d4bad3549eb9ed03b98be1e77852de
zRCoder <43688805@qq.com>
2024-11-23 02:08:29 +0800

    feat: 添加配置管理,数据库初始化

---
+++ pkg/config/config.go
@@ -0,0 +1,94 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+// Config 框架配置结构体
+type Config struct {
+	AppName string `mapstructure:"app_name`
+	HTTP    struct {
+		Addr         string        `mapstructure:"addr"`
+		ReadTimeout  time.Duration `mapstructure:"read_timeout"`
+		WriteTimeout time.Duration `mapstructure:"write_timeout"`
+	} `mapstructure:"http"`
+
+	Database struct {
+		Host            string        `mapstructure:"host"`
+		Port            int           `mapstructure:"port"`
+		User            string        `mapstructure:"user"`
+		Password        string        `mapstructure:"password"`
+		DBName          string        `mapstructure:"dbname"`
+		Charset         string        `mapstructure:"charset"`
+		ParseTime       bool          `mapstructure:"parseTime"`
+		Loc             string        `mapstructure:"loc"`
+		ReadHosts       []string      `mapstructure:"read_hosts"`
+		WriteHost       string        `mapstructure:"write_host"`
+		MaxIdleConns    int           `mapstructure:"max_idle_conns"`
+		MaxOpenConns    int           `mapstructure:"max_open_conns"`
+		ConnMaxLifetime time.Duration `mapstructure:"conn_max_lifetime"`
+	} `mapstructure:"database"`
+
+	Logger struct {
+		Level    string `mapstructure:"level"`
+		Output   string `mapstructure:"output"`
+		FielPath string `mapstructure:"filepath"`
+	} `mapstructure:"logger"`
+
+	Cache struct {
+		Type     string `mapstructure:"type"`
+		Addr     string `mapstructure:"addr"`
+		Password string `mapstructure:"password"`
+		DB       int    `mapstructure:"db"`
+	} `mapstructure:"cache"`
+}
+
+// LoadConfig 加载配置
+func LoadConfig(configPath string) (*Config, error) {
+	viper.SetConfigFile(configPath)
+	viper.SetConfigType("yaml")
+
+	// 设置默认配置
+	viper.SetDefault("app_name", "goin-app") // 默认应用名
+	viper.SetDefault("http.addr", ":8080")
+	viper.SetDefault("http.read_timeout", "60s")
+	viper.SetDefault("http.write_timeout", "60s")
+	viper.SetDefault("database.host", "localhost")
+	viper.SetDefault("database.port", 3306)
+	viper.SetDefault("database.user", "root")
+	viper.SetDefault("database.password", "")
+	viper.SetDefault("database.dbname", "mydatabase")
+	viper.SetDefault("database.charset", "utf8mb4")
+	viper.SetDefault("database.parseTime", true)
+	viper.SetDefault("database.loc", "Local")
+	viper.SetDefault("database.max_idle_conns", 10)
+	viper.SetDefault("database.max_open_conns", 100)
+	viper.SetDefault("database.conn_max_lifetime", "30m")
+	viper.SetDefault("logger.level", "debug")
+	viper.SetDefault("logger.output", "console")
+	viper.SetDefault("cache.type", "none") // 默认不使用缓存
+
+	// 读取配置文件
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	// 获取 Appname 配置，并将其转换为环境变量前缀
+	appName := viper.GetString("app_name")
+	envPrefix := strings.ToUpper(strings.ReplaceAll(appName, "-", "_"))
+
+	// 绑定环境变量,允许环境变量覆盖配置
+	viper.SetEnvPrefix(envPrefix)
+	viper.AutomaticEnv()
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
+	}
+
+	return &cfg, nil
+}
---
+++ pkg/database/database.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/zR-Zr/goin/pkg/config"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var (
+	DB *gorm.DB
+)
+
+// Init 初始化数据库链接
+func Init(cfg *config.Config) error {
+	// 组装 DSN
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=%t&loc=%s",
+		cfg.Database.User, cfg.Database.Password, cfg.Database.Host, cfg.Database.Port, cfg.Database.DBName, cfg.Database.Charset, cfg.Database.ParseTime, cfg.Database.Loc)
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // 慢 SQL 阈值
+			LogLevel:      logger.Info, // 日志级别
+			Colorful:      true,        // 彩色打印
+		},
+	)
+
+	// 链接数据库
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	sqlDB, err := DB.DB()
+	if err != nil {
+		return err
+	}
+
+	sqlDB.SetMaxIdleConns(cfg.Database.MaxIdleConns)
+	sqlDB.SetMaxOpenConns(cfg.Database.MaxOpenConns)
+	sqlDB.SetConnMaxLifetime(cfg.Database.ConnMaxLifetime)
+
+	return nil
+}
